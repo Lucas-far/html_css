1528d0d369637ba3bb4618e0a14b5abb9751bb85
luksadmin <lukszwei2@gmail.com>
2021-01-12 21:56:56 -0300

    Modules storing knowledge collected over the subjects of HTML and CSS

---
+++ inputs.py
@@ -0,0 +1,107 @@
+
+
+"""
+Módulo:
+
+Objetivo:
+"""
+
+def input_type_choice():
+    """
+    <div>
+        <select id="questions" size="7" multiple>
+            <option value="answer1">{% trans "Uma droga!" %}</option>
+            <option value="answer2">{% trans "Tudo bem!" %}</option>
+            <option value="answer3">{% trans "Uma maravilha!" %}</option>
+            <option value="answer4">{% trans "Não sei dizer!" %}</option>
+        </select>
+    </div>
+
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="exampleFormControlSelect1">Campo para selecionar um gênero</label>
+            <label>Escolha um gênero</label>
+            <select class="form-control" id="exampleFormControlSelect1" name="gender">
+                <option>escolher...</option>
+                <option>feminino</option>
+                <option>masculino</option>
+                <option>outro</option>
+            </select>
+        </div>
+    </div>
+    """
+
+def input_type_email():
+    """
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="email">Campo para escrever e-mail</label>
+            <input type="email" class="form-control" id="email" max="100" min="2" name="email" placeholder="seu e-mail" size="20" required>
+        </div>
+    </div>
+    """
+
+def input_type_file():
+    """
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="avatar">Campo para fazer upload do seu avatar</label>
+            <label>Forneça seu avatar</label>
+            <input type="file" class="form-control-file" id="avatar" name="avatar">
+        </div>
+    </div>
+    """
+
+def input_type_number():
+    """
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="age">Campo para escrever idade</label>
+            <input type="number" class="form-control" id="age" name="age" min="1" max="120" placeholder="sua idade" size="20" required>
+        </div>
+    </div>
+    """
+
+def input_type_password():
+    """
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="password">Campo para escrever senha</label>
+            <input type="password" class="form-control" id="password" max="100" min="2" name="password" placeholder="sua senha" size="20" required>
+        </div>
+    </div>
+    """
+
+def input_type_text():
+    """
+    <div class="row mt-5">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="first_name">Campo para escrever o primeiro nome</label>
+            <input type="text" class="form-control" id="first_name" max="100" min="2" name="first_name" placeholder="seu primeiro nome" size="20" required>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="last_name">Campo para escrever o sobrenome</label>
+            <input type="text" class="form-control" id="last_name" max="100" min="2" name="last_name" placeholder="seu sobrenome" size="20"  required>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="form-group ma mb-2">
+            <label class="sr-only" for="username">Campo para escrever o nome de usuário/apelido</label>
+            <input type="text" class="form-control" id="username" max="100" min="2" name="username" placeholder="seu nome de usuário" size="20" required>
+        </div>
+    </div>
+    """
+
+def input_type_textarea():
+    """
+    <div class="row">
+        <div class="form-group ma mb-2 col-7 col-sm-7 col-md-7 col-lg-7 col-xl-7">
+            <label class="sr-only" for="brief_description">Campo para falar algo sobre você</label>
+            <textarea class="form-control" id="brief_description" name="brief_description" placeholder="Fale algo sobre você" rows="7" cols="40" required></textarea>
+        </div>
+    </div>
+    """
---
+++ tags.py
@@ -0,0 +1,181 @@
+
+
+"""
+Módulo: tags.py
+
+Objetivo:
+         registrar tags que eu já vi sobre HTML
+"""
+
+from random import choice
+alfabeto = [*'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'.split(',')]
+inteiros = [*'1,2,3,4,5,6,7,8,9'.split(',')]
+resultado = []
+index = 0
+while index < 5:
+    resultado.extend(choice(alfabeto))
+    resultado.extend(choice(inteiros))
+    index += 1
+for string in resultado:
+    print(string, end='')
+
+# <!---->
+def c4n1i8d4j1():
+    """
+    Objetivo:      tag única que permite criar observações sobre seu código no módulo HTML
+    Palavra chave tags: comentarista
+    """
+
+# <!DOCTYPE html>
+def y3k8v5h3f8():
+    """
+    Objetivo:      tag declaradora da criação de um módulo HTML (sempre a 1a escrita)
+    Palavra chave tags: declaradora
+    """
+
+# <a href="#"></a>
+def l5n9t4o4e5():
+    """
+    Objetivo:      tag conversora de texto comum para um texto de link
+    Palavra chave tags: redirecionadora
+    """
+
+# <b></b>
+def c5s3b2f7x5():
+    """
+    Objetivo:      tag formatadora de fonte para negrito, sem ênfase ao motor de busca
+    Palavra chave tags: negrito
+    """
+
+# <body></body>
+def o9h5b5v6d4():
+    """
+    Objetivo: Tag armazenadora de dados visuais em um módulo HTML
+    Palavra chave tags: corporal
+    """
+
+# <br>
+def j5e4l3a7l2():
+    """
+    Objetivo:      tag geradora de uma quebra de linha, caso seja usada 1, ou geradora de parágrafos, caso 2+
+    Palavra chave tags: quebra linha
+    """
+
+# <em></em>
+def r5k2p8n6p1():
+    """
+    Objetivo:      tag formatadora de fonte para itálico, com ênfase ao motor de busca
+    Palavra chave tags: italica motor
+    """
+
+# <h1></h1>...<h6> </h6>
+def i2e2b7k1a6():
+    """
+    Objetivo:      tag formatadora de variações de títulos em um módulo HTML
+    Palavra chave tags: titular
+    """
+
+# <head></head>
+def h9p6a7z4c9():
+    """
+    Objetivo:      tag armazenadora de dados não visuais de um módulo HTML
+    Palavra chave tags: teorica
+    """
+
+# <html lang=""></html>
+def tag():
+    """
+    Objetivo:      tag iniciadora e finalizadora de um módulo HTML
+    Palavra chave tags: inicializadora
+    """
+
+# <hr>
+def n3a5d4t6j2():
+    """
+    Objetivo:      tag geradora de uma linha (se usado antes de uma tag, a linha vai acima do texto)
+    Palavra chave tags: tracejadora
+    """
+
+# <i></i>
+def v5v6l4g9n2():
+    """
+    Objetivo:      tag formatadora de fonte para itálico, sem ênfase ao motor de busca
+    Palavra chave tags: italica
+    """
+
+# <img alt="" src="">
+def q7z7k4a1q7():
+    """
+    Objetivo:      tag para imagens, dependente de atributos, que nesse caso, são identificadoras da rota da imagem
+    Palavra chave tags: pixels
+    """
+
+# <link href="" rel="stylesheet">
+def s4h4s6l2e3():
+    """
+    Objetivo:      tag dependente de atributos, que nesse caso, são vinculadoras dos módulos CSS com o HTML
+    Palavra chave tags: referenciadora css
+    """
+
+# <meta charset="utf-8">
+def q7b8j3e2e6():
+    """
+    Objetivo:      tag dependente de atributos, para configurar caracteres especiais ao módulo HTML
+    Palavra chave tags: referenciadora caractere
+    """
+
+# <meta name="author" content="">
+def o6o5m3q9u9():
+    """
+    Objetivo:      tag dependente de atributos, para configurar o autor do módulo HTML
+    Palavra chave tags: autora
+    """
+
+# <meta name="description" content="">
+def a6d1s1k5w1():
+    """
+    Objetivo:      tag dependente de atributos, para configurar o tipo de conteúdo do módulo HTML
+    Palavra chave tags: descritiva
+    """
+
+# <meta name="keywords" content="">
+def a8d9f5i9r7():
+    """
+    Objetivo:      tag dependente de atributos, para configurar palavras-chaves do módulo HTML
+    Palavra chave tags: chaves
+    """
+
+# <ol><li></li></ol>
+def e4q8w1e8u9():
+    """
+    Objetivo:      tag geradora de uma lista ordenada com padrão numérico
+    Palavra chave tags: lista ordenada
+    """
+
+# <p></p>
+def u2g9f1a7g2():
+    """
+    Objetivo:      tag geradora de parágrafos
+    Palavra chave tags: paragrafo
+    """
+
+# <strong></strong>
+def e2b3s5f5d5():
+    """
+    Objetivo:      tag formatadora de fonte para negrito, com ênfase ao motor de busca
+    Palavra chave tags: negrito motor
+    """
+
+# <title></title>v
+def j3y2r8i5w7():
+    """
+    Objetivo:      tag entituladora da aba de um módulo HTML criado
+    Palavra chave tags: rotuladora
+    """
+
+# <ul><li></li></ul>
+def z6v1k7f3p4():
+    """
+    Objetivo:      tag criadora de lista não ordenada com padrão pontilhado
+    Palavra chave tags: lista pontilhada
+    """
